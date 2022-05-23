548681f1c77d8cced2352b940b8c4c5c5fa22434
sslivkoff <stormslivkoff@gmail.com>
2022-05-23 00:09:19 -0700

    only define spec types if type checking

--- toolcache/spec.py
+++ toolcache/spec.py
@@ -1,9 +1,12 @@
+from __future__ import annotations
+
 import typing
-from typing_extensions import Literal
 
-from . import cachetypes
+if typing.TYPE_CHECKING:
+    from typing_extensions import Literal
+
+    from . import cachetypes
 
 
-CommonCachetypeName = Literal['disk', 'memory', 'null']
-CachetypeSpec = typing.Union[CommonCachetypeName, 'cachetypes.BaseCache']
-
+    CommonCachetypeName = Literal['disk', 'memory', 'null']
+    CachetypeSpec = typing.Union[CommonCachetypeName, 'cachetypes.BaseCache']
